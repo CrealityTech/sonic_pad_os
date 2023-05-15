9e6de794e06e727f50593ad2429e950b84c8859f
ykf <ykf@creality.com>
2023-05-15 11:48:24 +0800

    add .gitignore and prepare.sh

---
+++ scripts/prepare.sh
@@ -0,0 +1,12 @@
+rm lichee/arisc/ar100s/tools/toolchain.tar.bz2
+rm lichee/brandy-2.0/tools/toolchain/gcc-linaro-7.2.1-2017.11-x86_64_arm-linux-gnueabi.tar.xz
+rm lichee/brandy-2.0/tools/toolchain/riscv64-linux-x86_64-20200528.tar.xz
+rm lichee/arisc/coco/coco/tools/d10_toolchain.tar.bz2
+rm lichee/arisc/coco/coco/tools/openrisc_toolchain.tar.bz2
+rm package/cortana/cortana-sdk/libs/libskype_call.so
+cat lichee/arisc/ar100s/tools/toolchain.tar.bz2_part* >> lichee/arisc/ar100s/tools/toolchain.tar.bz2
+cat lichee/brandy-2.0/tools/toolchain/gcc-linaro-7.2.1-2017.11-x86_64_arm-linux-gnueabi.tar.xz_part* >> lichee/brandy-2.0/tools/toolchain/gcc-linaro-7.2.1-2017.11-x86_64_arm-linux-gnueabi.tar.xz
+cat lichee/brandy-2.0/tools/toolchain/riscv64-linux-x86_64-20200528.tar.xz_part* >> lichee/brandy-2.0/tools/toolchain/riscv64-linux-x86_64-20200528.tar.xz
+cat lichee/arisc/coco/coco/tools/d10_toolchain.tar.bz2_part* >> lichee/arisc/coco/coco/tools/d10_toolchain.tar.bz2
+cat lichee/arisc/coco/coco/tools/openrisc_toolchain.tar.bz2_part* >> lichee/arisc/coco/coco/tools/openrisc_toolchain.tar.bz2
+cat package/cortana/cortana-sdk/libs/libskype_call.so_part* >> package/cortana/cortana-sdk/libs/libskype_call.so
